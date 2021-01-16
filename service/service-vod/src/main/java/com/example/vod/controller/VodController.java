d814822678fe7f76629bf54cc2a34294f1423c86
谢明月 <1743185983@qq.com>
2021-01-16 22:57:36 +0800

    视频上传

---
+++ service/service-vod/src/main/java/com/example/vod/controller/VodController.java
@@ -0,0 +1,59 @@
+package com.example.vod.controller;
+
+import com.aliyuncs.DefaultAcsClient;
+import com.aliyuncs.vod.model.v20170321.DeleteVideoRequest;
+import com.example.common.R;
+import com.example.servicebase.exceptionhandler.GuliException;
+import com.example.vod.Utils.ConstantVodUtils;
+import com.example.vod.Utils.InitVodCilent;
+import com.example.vod.service.VodService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RestController
+@RequestMapping("/eduvod/video")
+@CrossOrigin
+public class VodController {
+
+    @Resource
+    private VodService vodService;
+
+    //上传视频到阿里云
+    @PostMapping("/uploadAliVideo")
+    public R uploadAliVideo(MultipartFile file) {
+        //返回上传视频id
+        String videoId = vodService.uploadVideoAly(file);
+        return R.ok().data("videoId",videoId);
+    }
+
+    //根据视频id删除阿里云视频
+    @PostMapping("/removeAlyVideo/{id}")
+    public R removeAlyVideo(@PathVariable String id) {
+        try {
+            //初始化对象
+            DefaultAcsClient client = InitVodCilent.initVodClient(ConstantVodUtils.ACCESS_KEY_ID, ConstantVodUtils.ACCESS_KEY_SECRET);
+            //创建删除视频request对象
+            DeleteVideoRequest request = new DeleteVideoRequest();
+            //向request设置视频id
+            request.setVideoIds(id);
+            //调用初始化对象的方法实现删除
+            client.getAcsResponse(request);
+            return R.ok();
+        }catch(Exception e) {
+            e.printStackTrace();
+            throw new GuliException(20001,"删除视频失败");
+        }
+    }
+
+    //删除多个阿里云视频的方法
+    //参数多个视频id  List videoIdList
+    @PostMapping("/delete-batch")
+    public R deleteBatch(@RequestParam("videoIdList") List<String> videoIdList) {
+        vodService.removeMoreAlyVideo(videoIdList);
+        return R.ok();
+    }
+}
---
+++ service/service-vod/src/main/java/com/example/vod/service/VodService.java
@@ -0,0 +1,16 @@
+package com.example.vod.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+public interface VodService {
+    //上传视频到阿里云
+    String uploadVideoAly(MultipartFile file);
+
+    /**
+     * 删除多个视频
+     * @param videoIdList
+     */
+    void removeMoreAlyVideo(List videoIdList);
+}
